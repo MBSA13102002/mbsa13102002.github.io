8df4b1f4c128c55d5b8564a7dbf5d56b4fc39343
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2025-01-23 02:35:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-        document.body.innerHTML = "<h1>Mobile me nahi , Please open this website on a laptop madam ji. Ik you will pull off some stunt like this.</h1>";
-    } else {
-        // Load the body normally
-        document.body.style.display = "block";
-    }
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     if (/Mobi|Android/i.test(navigator.userAgent)) {
+//         document.body.innerHTML = "<h1>Mobile me nahi , Please open this website on a laptop madam ji. Ik you will pull off some stunt like this.</h1>";
+//     } else {
+//         // Load the body normally
+//         document.body.style.display = "block";
+//     }
+// });
 
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
